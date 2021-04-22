e5fc05752ae7177e29cdcaae2320d4ca6636b94e
chilipepperhott <elijah.sirius@protonmail.com>
2021-04-22 12:07:07 -0600

    Added michelle name to checks

--- src/checks.rs
+++ src/checks.rs
@@ -3,12 +3,13 @@ use serenity::framework::standard::{macros::check, Reason};
 use serenity::model::prelude::Message;
 
 const OWNERNAME: &str = "ChiliPepperHott#4147";
-const LEE_NAME: &str = "ToxicGarbage#191"; 
+const LEE_NAME: &str = "ToxicGarbage#191";
+const MICHELLE_NAME: &str = "Mochi#3133";
 
 #[check("Owns bot")]
 pub async fn is_bot_owner_or_guest(ctx: &Context, msg: &Message) -> Result<(), Reason> {
     let name = format!("{}#{}", msg.author.name, msg.author.discriminator);
-    if !(name == LEE_NAME || name == OWNERNAME) {
+    if !(name == LEE_NAME || name == OWNERNAME || name == MICHELLE_NAME) {
         return Err(Reason::UserAndLog {
             user: msg.author.name.to_owned(),
             log: "You are not the bot owner".to_string(),
